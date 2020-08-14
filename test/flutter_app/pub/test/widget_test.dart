887314b20ce191340c2532d40acdf53f0e080c28
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-08-14 17:16:55 -0300

    Ajustes

--- test/flutter_app/pub/test/widget_test.dart
+++ test/flutter_app/pub/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pub/main.dart';
+import '../lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
