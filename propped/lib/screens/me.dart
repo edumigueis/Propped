0b61b285ed809136eb761050f4a41a9b7ac4874c
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-10-20 17:31:31 -0300

    Finished Profile Page

--- propped/lib/screens/me.dart
+++ propped/lib/screens/me.dart
@@ -11,7 +11,7 @@ enum SingingCharacter { All, Woman, Men, Genderless }
 
 class _Me extends State<Me> {
   SingingCharacter _character = SingingCharacter.Woman;
-
+  List supItems = ["About Propped", "Terms and Conditions", "Privacy Policy", "Partners", "Guides"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -108,16 +108,18 @@ class _Me extends State<Me> {
                               width: 40,
                               margin: EdgeInsets.symmetric(horizontal: 20),
                               decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.all(Radius.circular(60)),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(60)),
                                   image: DecorationImage(
                                       image: NetworkImage(
-                                          "https://images.pexels.com/photos/2101839/pexels-photo-2101839.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"))),
+                                          "https://cdn4.iconfinder.com/data/icons/square-world-flags/180/flag_us_america_united_states_square-512.png"))),
                             ),
                             Container(
                               margin: EdgeInsets.symmetric(vertical: 20),
-                              child: Text("United States of America (\$ USD)", style: TextStyle(
-                                fontSize: 16.7
-                              ),),
+                              child: Text(
+                                "United States of America (\$ USD)",
+                                style: TextStyle(fontSize: 16.7),
+                              ),
                             )
                           ],
                         ),
@@ -231,6 +233,53 @@ class _Me extends State<Me> {
                     ),
                   ],
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, bottom: 40),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Text("Location",
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                fontFamily: 'Ubuntu',
+                                fontWeight: FontWeight.w600,
+                                color: Color.fromRGBO(40, 40, 40, 1))),
+                      ),
+                      ListView.builder(
+                        itemCount: 5,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            height: 60,
+                            decoration: new BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(
+                                        width: 1.0,
+                                        style: BorderStyle.solid,
+                                        color: Colors.black26))),
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text(
+                                    supItems[index],
+                                    style: TextStyle(fontSize: 17),
+                                  ),
+                                  Icon(Icons.arrow_forward_ios, color: Color.fromRGBO(30, 30, 30, 1),)
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                      )
+                    ],
+                  ),
+                )
               ],
             )));
   }
