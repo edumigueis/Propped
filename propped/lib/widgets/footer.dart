c9040ae63b5ed548e7abdc1419a97c027aff7d5a
digo-smithh <smithrodrigues08@gmail.com>
2020-10-22 17:48:00 -0300

    edit

--- propped/lib/widgets/footer.dart
+++ propped/lib/widgets/footer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class Footer extends StatelessWidget {
   @override
