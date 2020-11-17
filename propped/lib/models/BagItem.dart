301f24409a87d5b462fc8da3498f425872d917b5
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-11-17 16:57:44 -0300

    Create BagItem.dart

---
+++ propped/lib/models/BagItem.dart
@@ -0,0 +1,9 @@
+import 'package:propped/models/Product.dart';
+
+class BagItem{
+  final Product product;
+  final int quantity;
+  final int size;
+
+  BagItem({this.product, this.quantity, this.size});
+}
